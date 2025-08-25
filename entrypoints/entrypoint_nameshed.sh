7e6942ec837016add0eed5c0271fa486ea7b63b1
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2025-08-25 21:12:19 +0200

    Fix dnst location and state/config file names, and force resigning before expiration.

--- entrypoints/entrypoint_nameshed.sh
+++ entrypoints/entrypoint_nameshed.sh
@@ -7,7 +7,9 @@ chown --recursive nameshed:nameshed /nameshed
 # No zone file registration support yet in Nameshed, invoke dnst manually for
 # now
 if [ ! -f /tmp/tld.conf ]; then
-    /nameshed/bin/dnst keyset -c /tmp/tld.conf create -n tld -s /tmp/tld.state
+    # Currently nameshed assumes zone config and state files are located in /tmp/
+    # named /tmp/<zone>.cfg and /tmp/<zone>.state so we have to match that here.
+    /tmp/dnst keyset -c /tmp/tld.cfg create -n tld -s /tmp/tld.state
 
     # the name 'hsmrelay' has to match the hard-coded name in nameshed/src/manager.rs
     # the hostname "nameshed-hsm-relay" has to match the hostname assigned in
@@ -16,7 +18,7 @@ if [ ! -f /tmp/tld.conf ]; then
     # dnst kmip server settings and nameshed env vars.
     # the username and passwords have to match between dnst kmip server
     # settings and nameshed env vars.
-    /nameshed/bin/dnst keyset -c /tmp/tld.conf kmip add-server \
+    /tmp/dnst keyset -c /tmp/tld.cfg kmip add-server \
         hsmrelay \
         nameshed-hsm-relay \
         --credential-store /tmp/tld.creds \
@@ -26,7 +28,12 @@ if [ ! -f /tmp/tld.conf ]; then
         --port 5696 \
         --key-label-prefix NS \
         --key-label-max-bytes 255
-    /nameshed/bin/dnst keyset -c /tmp/tld.conf init
+
+    # Force frequent resigning by setting a low DNSKEY RRSIG expiration.
+    /tmp/dnst keyset -c /tmp/tld.cfg set dnskey-lifetime 15s
+    /tmp/dnst keyset -c /tmp/tld.cfg set dnskey-remain-time 10s
+
+    /tmp/dnst keyset -c /tmp/tld.cfg init
 fi
 
 # No config file parsing support yet in Nameshed, use env vars for now
